f623fb3ab9d0457566c514b9cf78e4ff57c2e740
Siddhant Jain <138900970+jainsiddhantofficial@users.noreply.github.com>
2025-10-25 14:45:03 +0530

    Delete 2137-final-value-of-variable-after-performing-operations directory

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int finalValueAfterOperations(String[] operations) {
-        int x = 0;
-        for (String op : operations) {
-            x += op.charAt(1) == '+' ? 1 : -1;
-        }
-        return x;
-    }
-}
